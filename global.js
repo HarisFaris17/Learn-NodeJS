9d95212589c629ea2f083c0094e4ee17a8e74adc
HarisFaris17 <harisfaris17@gmail.com>
2022-09-02 13:19:00 +0700

    global object

---
+++ global.js
@@ -0,0 +1,5 @@
+// node.js gives Javascript global object. properties dari global lebih mengarah kepada environment 
+// global variable contains all the function, objek that are accessible throughout the file.
+// in simple explanation, all function, object, class that this file doesn't need to import is in global variable
+
+console.log(Object.getOwnPropertyNames(global))
