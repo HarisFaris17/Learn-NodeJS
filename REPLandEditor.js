2a9281594d91e1404e1593ea77d662ebcb1bd5fd
HarisFaris17 <harisfaris17@gmail.com>
2022-09-02 12:35:49 +0700

    REPL and Editor mode

---
+++ REPLandEditor.js
@@ -0,0 +1,5 @@
+// we can use REPL ( remember command windows in matlab/octave) in node.js just by typing "node" in cmd
+// every enter in REPL will display the returned value of the code entered. And also display all console.log
+// if we want to coding in long rows, hence we should switch to editor mode
+// to enter editor mode, type ".editor" while we are in REPL state
+// to exit REPL mode type ".exit"
